67220a1ae839eb7f91dda326b1ce5540cdcaee42
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2024-12-23 17:08:24 +0600

    first commit

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h2 className='text-red-700'>Update a Coffee</h2>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
